940f0574a55b6b977fac1130f63ee2a70c6469df
VanshGarg208 <vanshgarg1782006@gmail.com>
2025-03-08 13:04:42 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n);
-void printArray(int arr[], int n);
-
-
-
-void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        minIndex = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
+
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 }
 
 void printArray(int arr[], int n) {
-    int i;
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    for (int i = 0; i < n; i++) {
+        printf("%d", &arr[i]);
     }
-    printf("\n"); 
 }
